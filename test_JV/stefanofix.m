bd72df2a154497240b199cd1649f3bfac109179c
John Voight <jvoight@gmail.com>
2020-01-29 08:45:35 -0800

    Something stupid

--- test_JV/stefanofix.m
+++ test_JV/stefanofix.m
@@ -4,7 +4,8 @@ _<x> := PolynomialRing(Rationals());
 load "test_JV/test_JV_2_ord_irred_notnormal.txt";
 // load "test_JV/test_JV_3_ord_notirr.txt";
 
-for d in data do
-  print d;
+for i := 1 to #data do
+  d := data[i];
+  print i, "of", #data, ":", d;
   time _ := pAdicToComplexRoots(d[1],d[2]);
 end for;
